4e63bbbda8f9d55488e0e2a9b8327bd18cf228df
YiwenCui <cyw1995519@gmail.com>
2020-11-04 21:15:19 -0500

    C++ lecture1 assignments

---
+++ L1.cpp
@@ -0,0 +1,21 @@
+
+// /* C++ basic lecture 1
+
+
+#include <iostream> 
+// int main() {
+//     std::cout << "I'm going to be the best programmer in the world";
+//     return 0;
+// }
+
+// int main() {
+//     std::cout << "Hello, I am ham!" << std::endl;
+//     std::cout << "Hello, Ham. I am Sandwich. I love you!" << std::endl;
+//     std::cout << "I love you too, Sandwhich!" << std::endl;
+//     return 0;
+// }
+char main() {
+    std::cout << "Changing return type, fingers crossed!"; 
+    
+    return 'a';
+}
