cc4b9fa24fb799f4f5d0112471ad3d850059a3cd
YiwenCui <cyw1995519@gmail.com>
2020-11-07 17:42:18 -0500

    cpp L2

---
+++ L2.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+    int a = 4;
+    char ivy = 'a';
+    bool is_she_pretty = true;
+    float pi = 3.14;
+    double x = 2.14;
+    unsigned short int b = -1;
+    unsigned long long int d = -1;
+
+    cout << b << endl;
+    cout << d << endl;
+    cout << x << endl;
+
+    return 0;
+
+}
