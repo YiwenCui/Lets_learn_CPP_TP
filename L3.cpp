08f2da8bd867b71e2f496c4a16c5d6e8d8888ee2
YiwenCui <cyw1995519@gmail.com>
2020-11-23 10:36:46 -0500

    L3

---
+++ L3.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+    cout << 1+1 << endl;
+    cout << 1-1 << endl;
+    cout << 2*2 << endl;
+    cout << 4/3 << endl;
+    cout << 4%3 << endl;
+    
