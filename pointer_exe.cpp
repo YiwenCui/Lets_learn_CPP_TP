e1ece5575b4d85bd8c7d2685bb4153a0b66928cd
YiwenCui <cyw1995519@gmail.com>
2020-12-27 15:22:35 -0500

    exercise

---
+++ pointer_exe.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+    int main(){
+        int B[2][3];
+        int (*p)[3] =B;
+        B[0][0] =2;
+        B[0][1] =3;
+        B[0][2] =6;
+        B[1][0] =4;
+        B[1][1] =5;
+        B[1][2] =8;
+        std::cout<< &B[0] <<endl;
+        std::cout<<B<<endl;
+        std::cout<<*B<<endl;
+        std::cout<<B[0]<<endl;
+        std::cout<< &B[0][0]<<endl;
+
+    }
+
+// int main()
+// {
+//     int a;
+//     int *p;
+//     a=10;
+//     p=&a;
+//     std::cout<< *p << endl;
+//     std::cout<< p<< endl;
+
+//     int b=20;
+//     *p=b; 
+//     std::cout<< *p <<endl;
+//     std::cout<< p <<endl;
+// }   
+   
+// int Increment(int a)
+//     {
+//         a =a+1;
+//         return a;
+//     }   
+// int main(){
+//     int a;
+//     int b;
+//     a=10;
+//     b= Increment(a);
+//     std::cout<<b<<endl;
+//     }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,28 @@
 #include <iostream> 
 using namespace std;
+int CalcCommonFactorOf(int x, int y);
+int NextCommonFactor(int a,int b);
 
-int main(){
+int i;
+int main(void)
+{
+    int sub;
+    CalcCommonFactorOf(100,50);
+    while ((sub= CalcCommonFactorOf(100,50))>0){
+        static int counter = 1;
+        
+        printf("Common factor %d is %d\n", counter++, sub);
+        continue;
+    }
+    return 0;
+}
+
+int CalcCommonFactorOf(int x, int y ){
+    for (i=std::min(x,y);i>=1;i--){
+        if((x%i ==0)&&(y%i ==0)){
+        }
+        
+    return i;    
+    }
+    
+}
